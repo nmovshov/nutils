1606929e871d19cf0559c1264f685218a7bb0e35
Naor Movshovitz <nmovshov@gmail.com>
2018-03-14 13:07:41 -0700

    Clean up startup.m.

--- mFiles/startup.m
+++ mFiles/startup.m
@@ -11,12 +11,6 @@ end
 switch mname
     case 'KZIN'
         set(groot,'DefaultFigurePosition',[555 290 768 576])
-    case 'PAK'
-        if verLessThan('matlab','8.4.0')
-            set(0,'DefaultFigurePosition',[460 299 632 474])
-        else
-            set(groot,'DefaultFigurePosition',[460 299 632 474])
-        end
     case 'NESUS'
         set(groot,'DefaultFigurePosition',[526 345 560 420])
     case 'OPUS'
