01b4c9d24a20299d88c9e71b11e2251a63696b23
Naor Movshovitz <nmovshov@gmail.com>
2018-04-13 22:03:43 -0700

    Improvements to mice, originaly written last millenium.

--- mFiles/mice.m
+++ mFiles/mice.m
@@ -1,8 +1,5 @@
 %% mice - an experiment (in behavioral science?)
 
-clear all
-close all
-clc
 %% --- Setup phase ---
 % An array of four "mice" objects, with fields for position and velocity,
 % both 2D vectors
@@ -18,12 +15,12 @@ mouse(4).velocity=[0 0];
 axis([0 1 0 1]);
 set(gca,'xtick',[],'ytick',[],'ycolor','w','xcolor','w')
 hold on
-m1=plot(mouse(1).position(1),mouse(1).position(2),'.b');
-m2=plot(mouse(2).position(1),mouse(2).position(2),'.r');
-m3=plot(mouse(3).position(1),mouse(3).position(2),'.g');
-m4=plot(mouse(4).position(1),mouse(4).position(2),'.y');
+m1=plot(mouse(1).position(1),mouse(1).position(2),'xb', 'linew', 2);
+m2=plot(mouse(2).position(1),mouse(2).position(2),'xr', 'linew', 2);
+m3=plot(mouse(3).position(1),mouse(3).position(2),'xg', 'linew', 2);
+m4=plot(mouse(4).position(1),mouse(4).position(2),'xy', 'linew', 2);
 m=[m1 m2 m3 m4];
-set(m,'erasemode','none','markersize',6)
+%set(m,'erasemode','none','markersize',6)
 
 %% Make them run
 safety=0;
