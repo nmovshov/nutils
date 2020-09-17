5be8ac1139a74a51a83cdf5cc52bc5a55f8abd79
Naor Movshovitz <nmovshov@gmail.com>
2020-09-17 12:23:15 -0700

    ngraf.nhists() convenience wrapper for comparing hists of multiple data sets.

---
+++ mFiles/+ngraf/nhists.m
@@ -0,0 +1,36 @@
+function [fh,ah,hh] = nhists(data,data2)
+%NHISTS Convenience wrapper to histogram with my favorite defaults.
+%   NHISTS(data) where data is a cell array just calls the built-in function
+%   histogram successively on data{i}, making sure hold is on and setting
+%   normalization to 'pdf', more useful than the default 'count' in my opinion,
+%   especially when comparing data sets.
+%
+%   NHISTS(data1,data2) where data1 and data2 are vectors is an alternative
+%   syntax, convenient for comparing two data sets.
+
+% Inputs
+if nargin == 0
+    fprintf('Usage:\n\tnhists(data1,data2)\n')
+    fprintf('where data1 and data2 are vectors, or\n')
+    fprintf('\tnhists(data)\n')
+    fprintf('where data is a cell array of vectors.\n')
+    return
+end
+narginchk(1,2)
+if nargin > 1 % hack for common case of two data sets
+    data1 = data;
+    validateattributes(data1,{'numeric'},{'vector'},'','data1')
+    validateattributes(data2,{'numeric'},{'vector'},'','data2')
+    data = {data1,data2};
+end
+validateattributes(data,{'cell'},{'vector'},'','data')
+assert(all(cellfun(@isvector,data)), 'nhists works on one-dimensional data.')
+
+% Call histogram on each data set
+[fh,ah] = ngraf.get_canvas(); % holds ah, fyi
+hh = gobjects(size(data));
+for k=1:length(data)
+    hh(k) = histogram(data{k});
+end
+
+end
