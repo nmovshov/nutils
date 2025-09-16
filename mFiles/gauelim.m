2bb9002426605d57568013421260bed4a78866be
Naor Movshovitz <nmovshov@gmail.com>
2025-09-15 20:51:23 -0700

    adding ero list output in gauelim

--- mFiles/gauelim.m
+++ mFiles/gauelim.m
@@ -10,6 +10,8 @@ end
 narginchk(1,1)
 nargoutchk(0,2)
 assert(length(size(A))==2,"A must be a matrix")
+ero = [];
+eroflag = nargout > 1;
 
 %% Gauss eliminate A (in place)
 [m,n] = size(A);
@@ -24,10 +26,26 @@ while (pr < m) && (pc < n)
             tmp = A(i,:);
             A(i,:) = A(pr,:);
             A(pr,:) = tmp;
+            if eroflag
+                s = sprintf("R%d<-->R%d",pr,i);
+                ero = [ero; s]; %#ok<AGROW>
+            end
         end
         for i=pr+1:m
-            f = A(i,pc)./A(pr,pc);
+            f = A(i,pc)/A(pr,pc);
             A(i,:) = A(i,:) - f*A(pr,:);
+            if eroflag && (f ~= 0)
+                if f == 1
+                    s = sprintf("R%d->R%d-R%d",i,i,pr);
+                elseif f == -1
+                    s = sprintf("R%d->R%d+R%d",i,i,pr);
+                elseif f > 0
+                    s = sprintf("R%d->R%d-(%s)R%d",i,i,strip(rats(f)),pr);
+                else
+                    s = sprintf("R%d->R%d+(%s)R%d",i,i,strip(rats(-f)),pr);
+                end
+                ero = [ero; s]; %#ok<AGROW>
+            end
         end
         pr = pr + 1;
         pc = pc + 1;
@@ -35,5 +53,4 @@ while (pr < m) && (pc < n)
 end
 
 %% ABYU
-ero = A; % placeholder for ero list
 end
